8e5c0b9f5b43a89faff7ed5a6f47f824ac146413
rizalnrrkhman <rizalnurrokhman@gmail.com>
2024-02-13 13:50:18 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Komparasi-Suhu-Sederhana
---
+++ komparasi suhu.py
@@ -0,0 +1,19 @@
+print ("Program Komparasi Suhu")
+print ('\n')
+C = input("Masukan Suhu dalam C = ")
+C = float(C)
+print ('\n')
+print ("Suhunya adalah ",C,"C")
+
+
+#R
+R = (4/5)*C
+print ("Suhu dalam R adalah ",R,"R")
+
+#F
+F = ((9/5)*C)+32
+print ("Suhu dalam F adalah ",F,"F")
+
+#K
+K = C + 273
+print ("Suhu dalam K adalah ",K,"K")
